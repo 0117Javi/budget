6462fd52af4c413729708c04e288bbb1233c4982
jj espinoza <j.j.espinozagomez@gmail.com>
2021-06-10 17:16:38 -0500

    act.18 code

--- Develop/service-work.js
+++ Develop/service-work.js
@@ -7,3 +7,15 @@ const FILES_TO_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "data-cache-v2";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(STATIC_CACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
